dc395a0078b1e244fc1fff9126bcb129f8abad6b
katey2658 <1145690747@qq.com>
2017-06-05 20:28:36 +0800

    对代理模式添加注释和进行部分修改

--- proxy/dynamicproxy/CglibProxy.java
+++ proxy/dynamicproxy/CglibProxy.java
@@ -12,18 +12,33 @@ import net.sf.cglib.proxy.MethodProxy;
 public class CglibProxy implements MethodInterceptor{
   private Object target;
 
+ /**
+  * 被代理的目标对象
+  * @param  Object target        [description]
+  * @return        [description]
+  */
   public Object getProxyInstance(Object target){
     this.target=target;
     Enhancer enhancer=new Enhancer();
     enhancer.setSuperclass(this.target.getClass());
     enhancer.setCallback(this);
+    //获得一个子类对象
     return enhancer.create();
   }
 
+/**
+ * 代理对象的方法被调用
+ * @param  Object      target        [description]
+ * @param  Method      method        [description]
+ * @param  Object[]    args          [description]
+ * @param  MethodProxy proxy         [description]
+ * @return             [description]
+ * @throws Throwable   [description]
+ */
   @Override
   public Object intercept(Object target,Method method,Object[] args,MethodProxy proxy)throws Throwable{
     //do something
-    Object obj=proxy.invokeSuper(target.args);
+    Object obj=proxy.invokeSuper(target,args);
     //do something
     return obj;
   }
--- proxy/dynamicproxy/SubjectProxyHandler.java
+++ proxy/dynamicproxy/SubjectProxyHandler.java
@@ -8,18 +8,31 @@ import java.lang.reflect.Method;
  */
 public class SubjectProxyHandler implements InvocationHandler {
 
+  /**
+   * 被代理对象
+   */
   private Object obj;
-
-
+  /**
+   * 传入代理对象：之后可以使用
+   * @param  Object obj           [description]
+   * @return        [description]
+   */
   public JDKDynamicSubjectProxy(Object obj){
     this.obj=obj;
   }
 
+/**
+ * 当代理对象被调用时候
+ * @param  Object    代理对象
+ * @param  Method    被调用方法
+ * @param  Object[] 参数对象
+ * @return  方法调用后的返回值
+ * @throws Throwable [description]
+ */
   public Object invoke(Object proxy,Method method,Object[] args) throws Throwable{
     //do something
     method.invoke(obj,args);
     //do something
     renturn null;
   }
-
 }
