17541dce41abc4ef4a102f233d03bb92cc73c8d9
katey2658 <1145690747@qq.com>
2017-07-17 20:14:23 +0800

    添加了命令模式

---
+++ factory/function/ProductFactory.java
@@ -0,0 +1,12 @@
+package  com.busyzero.chainofresponsibility.function;
+
+public class ProductFactory{
+  public static Product createProduct(String name){
+    switch(name){
+      case "loan":return new Loan();
+      case "stock":return new Stock();
+      case "bond":return new Bond();
+      default: throw new RuntimeException("No such Product "+name);
+    }
+  }
+}
